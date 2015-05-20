e9ba6866bc9d37c35384ea0078ea7f7c63338065
Ben Pearre <bwpearre@cumm024-0b02-dhcp-89.bu.edu>
2015-05-20 17:52:22 -0400

    loadall.m: working on data analysis...

---
+++ plexon/loadall.m
@@ -0,0 +1,46 @@
+clear;
+
+files = dir('stim*.mat');
+
+for i = 1:length(files)
+        
+       load(files(i).name);
+       dd{i} = data;
+       
+       if ~isfield(data, 'halftime') % old format does not scale saved data
+               scalefactor_V = 1/0.25;
+               scalefactor_i = 400;
+               data.data(:,1) = data.data(:,1) * scalefactor_V;
+               data.data(:,2) = data.data(:,2) * scalefactor_i;
+       end
+
+       
+       triggerchannel = 3;
+       triggerthreshold = 0.1;
+       aftertrigger = 0.006;
+       
+       triggertime = find(abs(data.data(:,triggerchannel)) > triggerthreshold);
+       if isempty(triggertime)
+               triggertime = 0;
+       else
+               triggertime = (triggertime(1) / data.fs);
+       end
+       beforetrigger = max(0, triggertime - 0.001);
+       
+       %disp('Bandpass-filtering the data...');
+       [B A] = butter(4, 0.6, 'low');
+       d = filter(B, A, data.data);
+       
+       scalefactor_V = 1/0.25; % V/V !!!!!!!!!!!!!!!!!!!!!!!
+       scalefactor_i = 400; % uA/mV, always!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+       times = (data.time - triggertime) * 1000; % milliseconds
+       u = find(data.time > beforetrigger & data.time < triggertime + aftertrigger);
+
+       
+       roi = round([ triggertime + 0.001  triggertime + 0.005 ] * data.fs);
+       
+       da(:,i) = d(roi(1):roi(2), 3);
+end
+
+
+times = [0:(size(da, 1)-1)] * 1000/data.fs;
