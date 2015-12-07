c19611745063551cc578803bba358a4d629325f4
bwpearre <bwpearre@gmail.com>
2015-12-07 17:57:36 -0500

    More versatile show_spike_train. Includes legend for spike-hunting.

--- plexon/show_spike_train.m
+++ plexon/show_spike_train.m
@@ -4,11 +4,12 @@ cd lw95rhp-2015-11-23;
 
 read_Intan_RHD2000_file;
 
-plot(t_amplifier*1e3-8760, amplifier_data([1 2 4], :)');
+plot(t_amplifier*1e3, amplifier_data(15, :)'/1e3);
 title('Spontaneous activity in Area X, EIROF array, +197 days');
 xlabel('milliseconds');
-ylabel('microvolts???');
-set(gca, 'XLim', [0 400]);
+ylabel('millivolts');
+legend('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16')
+%set(gca, 'XLim', [0 400]);
 
 set(gcf,'PaperPositionMode','auto'); 
 saveas(gcf, 'spiketrain.png');
